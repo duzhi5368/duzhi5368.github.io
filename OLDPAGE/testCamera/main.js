779f7995113e6db65e49c11c1361e4bf69588f62
DarkKnight <30592092+FKLostLand@users.noreply.github.com>
2020-06-18 14:25:44 +0800

    update

--- OLDPAGE/testCamera/main.js
+++ OLDPAGE/testCamera/main.js
@@ -1,3 +1,34 @@
+
+// 初始化
+windows.onload = function(){
+  var video = document.getElementById('video');
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var tracker = new tracking.ObjectTracker('face');
+  tracker.setInitialScale(2);
+  tracker.setStepSize(1);
+  tracker.setEdgesDensity(0.15);
+  tracking.track('#video', tracker, { camera: true });
+  tracker.on('track', function(event) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    event.data.forEach(function(rect) {
+      postRect(rect);
+      context.strokeStyle = '#a64ceb';
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+    });
+  });
+  //
+  var gui = new dat.GUI();
+  gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
+  gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
+  gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+}
+/*
 var points = [];
 var canvasP5;
 var videoP5;
@@ -41,11 +72,10 @@ function draw() {
     text(i, points[i].x, points[i].y);
   }
 
-  /*
   // 眼睛
   if (points.length > 24) {
     ellipse(points[20].x, points[20].y + 10, 50, 50);
     ellipse(points[24].x, points[24].y + 10, 50, 50);
   }
-  */
 }
+*/
