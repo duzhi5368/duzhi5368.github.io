ad6f0afd6ee6938032b4df320e3326250ca1a211
DarkKnight <30592092+FKLostLand@users.noreply.github.com>
2020-08-02 14:10:24 +0800

    new  diary

---
+++ OLDPAGE/testCamera/js/main.js
@@ -0,0 +1,323 @@
+const webcamElement = document.getElementById('webcam');
+const webcam = new Webcam(webcamElement, 'user');
+const modelPath = 'models';
+let displaySize;
+let canvas;
+let faceDetection;
+let bIsUpdateLogic = false;
+let bIsAutoImgProcessing = false;
+let minConfidence = 0.8;  // 人脸识别值
+
+// 开启摄像头
+$("#webcam-switch").change(function () {
+  if(this.checked){
+      webcam.start()
+          .then(result =>{
+             cameraStarted();
+             webcamElement.style.transform = "";
+             console.log("摄像头开启...");
+          })
+          .catch(err => {
+              displayError('摄像头开启失败', true);
+          });
+  }
+  else {        
+      cameraStopped();
+      webcam.stop();
+      console.log("摄像头关闭...");
+  }        
+});
+
+function cameraStarted(){
+  toggleContrl("detection-switch", true);
+  $("#errorMsg").addClass("d-none");
+  // 开启列表
+  if( webcam.webcamList.length > 1){
+    $("#cameraFlip").removeClass('d-none');
+  }
+}
+
+function cameraStopped(){
+  toggleContrl("detection-switch", false);
+  $("#errorMsg").addClass("d-none");
+  $("#cameraFlip").addClass('d-none');
+}
+
+function displayError(err = '', bIsShow){
+  if(bIsShow) {
+    if (err != '') {
+      $("#errorMsg").html(err);
+    }
+    $("#errorMsg").removeClass("d-none");
+  }else{
+    $("#errorMsg").addClass("d-none");
+  }
+}
+
+$('#cameraFlip').click(function() {
+    webcam.flip();
+    webcam.start()
+    .then(result =>{ 
+      webcamElement.style.transform = "";
+    });
+});
+
+// 摄像头启动初始化
+$("#webcam").bind("loadedmetadata", function () {
+  console.log("摄像头初始化...");
+  displaySize = { width:this.scrollWidth, height: this.scrollHeight }
+});
+
+// 开启面部识别
+$("#detection-switch").change(function () {
+  if(this.checked){
+    toggleContrl("box-switch", true);
+    toggleContrl("landmarks-switch", true);
+    toggleContrl("expression-switch", true);
+    toggleContrl("age-gender-switch", true);
+    toggleContrl("auto-snapshot", true);
+    toggleContrl("auto-img-processing", true);
+    toggleContrl("face-similarity", true);
+
+    $("#box-switch").prop('checked', true);
+    $(".loading").removeClass('d-none');
+
+    Promise.all([
+      faceapi.nets.ssdMobilenetv1.load(modelPath),
+      faceapi.nets.faceLandmark68TinyNet.load(modelPath),
+      faceapi.loadFaceRecognitionModel(modelPath)
+    ]).then(function(){
+      createCanvas();
+      startDetection();
+    })
+  }
+  else {
+    toggleContrl("box-switch", false);
+    toggleContrl("landmarks-switch", false);
+    toggleContrl("expression-switch", false);
+    toggleContrl("age-gender-switch", false);
+    toggleContrl("auto-snapshot", false);
+    toggleContrl("auto-img-processing",false);
+    toggleContrl("face-similarity", false);
+
+    clearInterval(faceDetection);
+    clearCanvas(canvas);
+  }        
+});
+
+function clearCanvas(thisCanvas){
+  if(thisCanvas != null){
+    setTimeout(function() {
+      thisCanvas.getContext('2d').clearRect(0, 0, thisCanvas.width, thisCanvas.height)
+    }, 1000);
+  }
+}
+
+function createCanvas(){
+  if( canvas == null )
+  {
+    canvas = faceapi.createCanvasFromMedia(webcamElement)
+    document.getElementById('canvas-container').append(canvas)
+    faceapi.matchDimensions(canvas, displaySize)
+  }
+}
+
+function toggleContrl(id, show){
+  if(show){
+    $("#"+id).prop('disabled', false);
+    $("#"+id).parent().removeClass('disabled');
+  }else{
+    $("#"+id).prop('checked', false).change();
+    $("#"+id).prop('disabled', true);
+    $("#"+id).parent().addClass('disabled');
+  }
+}
+
+function startDetection(){
+  faceDetection = setInterval(async () => {
+    const option = new faceapi.SsdMobilenetv1Options({ minConfidence })
+    const detections = await faceapi.detectAllFaces(webcamElement, option).withFaceLandmarks(true)
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    if($("#box-switch").is(":checked")){
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+    }
+    if($("#landmarks-switch").is(":checked")){
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    }
+
+    if(bIsUpdateLogic) {
+      if (detections.length == 1) {
+        displayError('', false);
+        var boundingBox = detections[0].alignedRect.box;
+        screenshot(boundingBox, 300);
+      } else if (detections.length > 1) {
+        displayError("摄像头内人脸不止一个", true);
+      } else {
+        displayError("摄像头内没有人脸", true);
+      }
+    }
+    
+    if(!$(".loading").hasClass('d-none')){
+      $(".loading").addClass('d-none')
+    }
+  }, 300)
+}
+
+
+$("#auto-snapshot").change(function () {
+  if(this.checked){
+    bIsUpdateLogic = true;
+  }
+  else {
+    bIsUpdateLogic = false;
+  }
+})
+
+$("#auto-img-processing").change(function () {
+  if(this.checked){
+    bIsAutoImgProcessing = true;
+  }
+  else {
+    bIsAutoImgProcessing = false;
+  }
+})
+
+function screenshot(boundingBox, picSize){
+  var video = document.getElementById('webcam');
+  var snapshotContainer = document.getElementById('snapshotContainer');
+  var snapshotCanvas = document.getElementById('snapshot');
+  if(snapshotCanvas == null) {
+    snapshotCanvas = document.createElement("canvas");
+    snapshotCanvas.setAttribute('width', '300');
+    snapshotCanvas.setAttribute('height', '300');
+    snapshotCanvas.setAttribute('id', 'snapshot');
+    snapshotContainer.append(snapshotCanvas);
+  }
+
+  drawSnapshot(snapshotCanvas, video, boundingBox, picSize);
+  grayscal(snapshotCanvas);
+}
+
+function drawSnapshot(snapshotCanvas, video, boundingBox, picSize){
+  var context = snapshotCanvas.getContext('2d');
+  context.fillRect(0, 0, snapshotCanvas.clientWidth, snapshotCanvas.clientHeight);
+  if(boundingBox == null || !bIsAutoImgProcessing) {
+    context.drawImage(video, 0, 0, snapshotCanvas.clientWidth, snapshotCanvas.clientHeight);
+  }else{
+    var xScale = picSize / boundingBox.width;
+    var yScale = picSize / boundingBox.height;
+    var scale = xScale > yScale ? yScale : xScale;
+    context.drawImage(video, boundingBox.x, boundingBox.y,
+        boundingBox.width, boundingBox.height,
+        (snapshotCanvas.clientWidth - picSize) / 2,
+        (snapshotCanvas.clientHeight - picSize) / 2,
+        boundingBox.width * scale, boundingBox.height * scale);
+  }
+}
+
+function grayscal(snapshotCanvas){
+  if(!bIsAutoImgProcessing){
+    return;
+  }
+  var context = snapshotCanvas.getContext('2d');
+  var imageSrc = context.getImageData(0, 0, snapshotCanvas.clientWidth, snapshotCanvas.clientHeight);
+  var dataSrc = imageSrc.data;
+  var len = dataSrc.length;
+  var i = 0;
+  var luma = 0;
+  for(; i < len; i += 4) {
+    luma = dataSrc[i] * 0.2126 + dataSrc[i+1] * 0.7152 + dataSrc[i+2] * 0.0722;
+    dataSrc[i] = dataSrc[i+1] = dataSrc[i+2] = luma;
+    dataSrc[i+3] = dataSrc[i+3];
+  }
+  context.putImageData(imageSrc, 0, 0);
+}
+
+$("#face-similarity").change(function () {
+  var snapshotCanvas = document.getElementById('snapshot');
+
+  var similaritySrcContainer = document.getElementById('similaritySrcContainer');
+  var similaritySrcCanvas = document.getElementById('similaritySrc');
+  if(similaritySrcCanvas == null) {
+    similaritySrcCanvas = document.createElement("canvas");
+    similaritySrcCanvas.setAttribute('width', '300');
+    similaritySrcCanvas.setAttribute('height', '300');
+    similaritySrcCanvas.setAttribute('id', 'similaritySrc');
+    similaritySrcContainer.append(similaritySrcCanvas);
+  }
+  var similarityDstContainer = document.getElementById('similarityDstContainer');
+  var similarityDstCanvas = document.getElementById('similarityDst');
+  if(similarityDstCanvas == null) {
+    similarityDstCanvas = document.createElement("canvas");
+    similarityDstCanvas.setAttribute('width', '300');
+    similarityDstCanvas.setAttribute('height', '300');
+    similarityDstCanvas.setAttribute('id', 'similarityDst');
+    similarityDstContainer.append(similarityDstCanvas);
+  }
+
+  var picListEle = document.getElementById('picList');
+  if(this.checked) {
+    const p = new Promise(function (resolve, reject) {
+      loadImgToCanvas(similarityDstCanvas, picListEle.value)
+      resolve()
+    });
+
+    p.then(function () {
+      copyCanvasToCanvas(snapshotCanvas, similaritySrcCanvas);
+      //loadImgToCanvas(similaritySrcCanvas, "./image/sheldon2.png")
+    }).then(function () {
+      Promise.all([
+        descriptor1 = faceapi.computeFaceDescriptor(similaritySrcCanvas),
+        descriptor2 = faceapi.computeFaceDescriptor(similarityDstCanvas)])
+          .then(function (descriptors) {
+            return faceapi.utils.round(faceapi.euclideanDistance(descriptors[0], descriptors[1]))
+          }).then(function (distanceResult) {
+            distanceResult = (1 - distanceResult) * 100
+          if(distanceResult == 100){
+            displaySorce("相似度：不明")
+          }else if(distanceResult < 20){
+            displaySorce("不会是同一个人，相似度：" + distanceResult)
+          }else if(distanceResult < 40){
+            displaySorce("不像是同一个人，相似度：" + distanceResult)
+          }else if(distanceResult < 60){
+            displaySorce("可能是同一个人，相似度：" + distanceResult)
+          }else if(distanceResult < 80){
+            displaySorce("基本是同一个人，相似度：" + distanceResult)
+          }else{
+            displaySorce("一定是同一个人，相似度：" + distanceResult)
+          }
+      })
+    })
+  }
+  else {
+    $("#errorMsg").addClass("d-none");
+    //clearCanvas(similaritySrcCanvas);
+    //clearCanvas(similarityDstCanvas);
+  }
+})
+
+function loadImgToCanvas(canvas, url){
+  new Promise( function(resolve, reject) {
+        var ctx = canvas.getContext('2d');
+        var img = new Image();
+        img.onload = function () {
+          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+        };
+        img.src = url;
+        resolve()
+      }
+  )
+}
+
+function copyCanvasToCanvas(src, dst){
+  var ctx = dst.getContext('2d');
+  ctx.drawImage(src, 0, 0);
+}
+
+function displaySorce(str){
+    if (str != '') {
+      $("#similarityMsg").html(str);
+    }
+    $("#similarityMsg").removeClass("d-none");
+}
